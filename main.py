a5306b37da0fbfedb6523c9202f69dd130bc0226
Unai <unai.devel@gmail.com>
2025-06-06 18:35:18 +0200

    Changed cors

--- main.py
+++ main.py
@@ -16,9 +16,14 @@ def create_db_and_tables():
 
 
 origins = [
-    "https://cinema.unaimunoz.dev",
-    "http://localhost:3000",
+    "http://localhost:3000",     # React (desarrollo)
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",     # Vite
+    "http://127.0.0.1:5173",
+    "http://localhost:5500",     # Live Server (HTML puro)
+    "http://127.0.0.1:5500",
 ]
+
 
 app.add_middleware(
     CORSMiddleware, 
